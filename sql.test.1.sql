3a8868a44accea5c99089ac0538d1be90415b038
DrewMurr2 <drewmurr2@gmail.com>
2020-09-23 17:04:29 -0500

    update sql tests

--- sql.test.1.sql
+++ sql.test.1.sql
@@ -1,1 +1,4 @@
--- Create a Sql query in MSSql or MySql with 2 columns: client_name, number_of_purchases
+-- Based on the test database, the database already has 3 tables, customers, products, and purchases
+-- An example: Select a as column_1, b as column_2 from ...
+
+-- Create a Sql Select Statement in MSSql or MySql with 2 columns: client_name, number_of_purchases
